055a1ccd130bc9cd16768701bb81db17d99605c6
Abel <avazquez18@jaumebalmes.net>
2019-10-23 16:49:37 +0200

    Creacion de la aplicación My Film Rates App

--- app/src/main/java/com/example/myfilmratesapp/AddFilm.java
+++ app/src/main/java/com/example/myfilmratesapp/AddFilm.java
@@ -2,13 +2,19 @@ package com.example.myfilmratesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class AddFilm extends AppCompatActivity {
     private EditText entradaTexto;
+    private ClipboardManager myClipboard;
+    private ClipData myClip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +23,21 @@ public class AddFilm extends AppCompatActivity {
     }
 
     public void startShare(View view) {
-
-        final EditText entradaTexto = (EditText) findViewById(R.id.editText_texto);
-        String textMessage = entradaTexto.getText().toString();
+        final EditText textoIntroducido = (EditText) findViewById(R.id.editText_texto);
+        String texto=textoIntroducido.getText().toString();
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, texto);
         sendIntent.setType("text/plain");
-
 // Verify that the intent will resolve to an activity
         if (sendIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(sendIntent);
         }
+    }
+
+
+    public void removeText(View view) {
+        final EditText entradaTexto = (EditText) findViewById(R.id.editText_texto);
+        entradaTexto.setText("");
     }
 }
